4f5c828dda1d085218fef65e7788007f10f3385a
Kyle Lindsay <kmlindsay00@gmail.com>
2016-05-26 17:44:41 -0600

    My Django Girls app, first commit

---
+++ testenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/kylelindsay/Python_Learning/Django_Learning/testenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
